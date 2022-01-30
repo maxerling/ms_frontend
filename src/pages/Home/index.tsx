9eb43f2da096d9e14bf6fe48f493cfa4843dbc08
Max Erling <max.d.erling@gmail.com>
2022-01-30 22:20:06 +0100

    homepage design

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,25 @@
 import axios from "axios";
 import { User } from "firebase/auth";
 import { useEffect, useState } from "react";
-import { Container } from "../../common/";
 import { Logo, NavBar, NavLink } from "../../components";
 import { IAuthContext, useAuth } from "../../contexts/AuthContext";
 import { Routine } from "../Routines";
-import { Spacer, Flex, Box, Button, Heading } from "@chakra-ui/react";
+import {
+  Spacer,
+  Flex,
+  Box,
+  Button,
+  Heading,
+  Container,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 interface OwnProps {}
 
 export const HomeP: React.FC<OwnProps> = () => {
   const { logOut, currentUser } = useAuth() as IAuthContext;
   const [currentRoutine, setCurrentRoutine] = useState<Routine>();
+  const [loading, setLoading] = useState(false);
   const navLinks = [
     {
       id: "1",
@@ -30,6 +39,7 @@ export const HomeP: React.FC<OwnProps> = () => {
 
   useEffect(() => {
     (async function () {
+      setLoading(true);
       const idToken = await currentUser?.getIdToken();
       const uid = (currentUser as User).uid;
       const options = {
@@ -52,38 +62,62 @@ export const HomeP: React.FC<OwnProps> = () => {
         options
       );
       setCurrentRoutine(routine.data as Routine);
+      setLoading(false);
     })();
   }, []);
   return (
     <>
-      <NavBar>
-        <Logo to="/" />
-        <Spacer />
-        <NavLink navLinks={navLinks} />
-      </NavBar>
-
-      <Container>
+      {!loading && (
         <>
-          {currentRoutine !== undefined ? (
-            <>
-              <div>Current Routine</div>
-              <>{currentRoutine.routineName}</>
-              {currentRoutine.activities.map((activity) => {
-                return (
-                  <div>
-                    <div>
-                      <>{activity.activityName}</>
-                      <>{activity.time_HHMM}</>
-                    </div>
-                  </div>
-                );
-              })}
-            </>
-          ) : (
-            <>No Current Routine</>
-          )}
+          <NavBar>
+            <Logo to="/" />
+            <Spacer />
+            <NavLink navLinks={navLinks} />
+          </NavBar>
+
+          <Container>
+            <VStack p={4}>
+              <Heading>
+                {currentRoutine ? "CURRENT ROUTINE" : "NO ROUTINE SELECTED "}
+              </Heading>
+            </VStack>
+            <Container p={2} borderRadius="lg" bg="gray.100" minH={800}>
+              <>
+                {currentRoutine && (
+                  <>
+                    <VStack p={2}>
+                      <Text fontWeight={"bold"} fontSize={"lg"}>
+                        {currentRoutine.routineName}
+                      </Text>
+                    </VStack>
+
+                    {currentRoutine.activities.map((activity) => {
+                      return (
+                        <Box
+                          p={2}
+                          borderRadius="lg"
+                          style={{ background: "white" }}
+                          m={1}
+                        >
+                          <Flex>
+                            <Text p={2} fontWeight={"semibold"}>
+                              {activity.activityName}
+                            </Text>
+                            <Spacer />
+                            <Text p={2} fontWeight={"semibold"}>
+                              {activity.time_HHMM}
+                            </Text>
+                          </Flex>
+                        </Box>
+                      );
+                    })}
+                  </>
+                )}
+              </>
+            </Container>
+          </Container>
         </>
-      </Container>
+      )}
     </>
   );
 };
